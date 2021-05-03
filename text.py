f65441f91b6e4ecc0981616163a89d3db46b4b01
Inchoata <taz.chiles@gmail.com>
2021-05-03 20:24:57 +0100

    Add level one text

--- text.py
+++ text.py
@@ -4,16 +4,47 @@ class Text():
 
 class LevelOneText():
     def __init__(self):
-        self.opening_text = ["You awaken, head pounding. There is a bright light",
-                             "shining onto your face. You open your eyes. You're",
-                             "alone in a bright, clinical room. You are tied to a",
-                             "surgical operating table. There are surgical ",
-                             "instruments on a table beside you. It looks like you",
-                             "could lean over and reach the tools with your mouth.",
-                             "What will you do?"]
-        self.options =      ["A) Cry for help.",
-                            "B) Attempt to lean over to reach a scalpel ",
-                            "   with your mouth.",
-                            "C) Look around."]
-        self.legend =       ["Legend:           ",
-                             " X - You are here!"]
+        self.opening_text =         ["You awaken, head pounding. There is a bright light",
+                                    "shining onto your face. You open your eyes. You're",
+                                    "alone in a bright, clinical room. You are tied to a",
+                                    "surgical operating table. There are surgical ",
+                                    "instruments on a table beside you. It looks like you",
+                                    "could lean over and reach the tools with your mouth.",
+                                    "What will you do?"]
+
+        self.actions =             ["A) Cry for help.",
+                                    "B) Attempt to lean over to reach a scalpel ",
+                                    "   with your mouth.",
+                                    "C) Look around."]
+
+        self.legend =              ["Legend:           ",
+                                    " X - You are here!"]
+
+        self.levelone_optionA =    ["This is some placeholder text for until I can be ",
+                                    "bothered to write the plot. User should have chosen",
+                                    "option A - Cry for help on the first screen."]
+
+        self.levelone_optionA_actions = ["A) Cry for help.",
+                                         "B) Attempt to lean over to reach a scalpel ",
+                                         "   with your mouth.",
+                                         "C) Look around."]
+
+        self.levelone_optionB =    ["This is some placeholder text for until I can be ",
+                                    "bothered to write the plot. User should have chosen",
+                                    "option B - Lean over and reach the scalpel on the ",
+                                    "first screen. Player should now have a scalpel in",
+                                    "the inventory"]
+
+        self.levelone_optionB_actions = ["A) Cry for help.",
+                                         "B) Attempt to lean over to reach a scalpel ",
+                                         "   with your mouth.",
+                                         "C) Look around."]
+
+        self.levelone_optionC =    ["This is some placeholder text for until I can be ",
+                                    "bothered to write the plot. User should have chosen",
+                                    "option C - Look around on the first screen."]
+
+        self.levelone_optionC_actions = ["A) Cry for help.",
+                                         "B) Attempt to lean over to reach a scalpel ",
+                                         "   with your mouth.",
+                                         "C) Look around."]
